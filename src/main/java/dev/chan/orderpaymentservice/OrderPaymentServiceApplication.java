7354cdae1d6617e3cce5617aef76581f12ba9234
seongchanhwang12 <tjdcksghkd12@gmail.com>
2025-10-01 20:27:25 +0900

    Initial commit

---
+++ src/main/java/dev/chan/orderpaymentservice/OrderPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.chan.orderpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/chan/orderpaymentservice/OrderPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chan.orderpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
